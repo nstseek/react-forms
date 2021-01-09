b1d4e3539eabd80dd78f1d9a975802fd183cffda
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2021-01-09 20:18:26 -0300

    Add script to control versions

---
+++ scripts/update-version.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const increaseVersion = process.argv[3];
+
+if (
+  increaseVersion !== '--major' &&
+  increaseVersion !== '--minor' &&
+  increaseVersion !== '--patch'
+) {
+  console.log(
+    'The third argument must indicate this release type\nPossible options: --major, --minor, --patch'
+  );
+  process.exit(1);
+}
+
+let file = fs.readFileSync(process.argv[2], { encoding: 'utf-8' });
+
+const versionStr = file.match(/"version": "[\d.]*"/g)[0];
+
+const versionNum = versionStr.replace('"version": ', '').replace(/"/g, '');
+
+const [majorVersion, minorVersion, patchVersion] = versionNum.split('.');
+
+const newVersion = `"version": "${
+  increaseVersion === '--major' ? Number(majorVersion) + 1 : majorVersion
+}.${increaseVersion === '--minor' ? Number(minorVersion) + 1 : minorVersion}.${
+  increaseVersion === '--patch' ? Number(patchVersion) + 1 : patchVersion
+}"`;
+
+file = file.replace(versionStr, newVersion);
+
+fs.writeFileSync(process.argv[2], file);
